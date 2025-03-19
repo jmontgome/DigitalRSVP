40f4bf22f8328f76555fb096672901497575d256
Jonathon Montgomery <jonmontgomery00@gmail.com>
2025-03-18 22:59:20 -0500

    View changes

--- DigitalRSVP.Database/Views/dbo.RsvpDetails.sql
+++ DigitalRSVP.Database/Views/dbo.RsvpDetails.sql
@@ -1,11 +1,21 @@
-go
-create view [dbo].[RsvpDetails]
+/****** Object:  View [dbo].[RsvpDetails]    Script Date: 3/18/2025 10:57:15 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+
+alter view [dbo].[RsvpDetails]
 as
 	select
 		inv.[WorkItemId] as InvitationId,
 		rsvp.[WorkItemId] as RsvpId,
 		inv.[EventId],
 		inv.[Name] as InvitationName,
+		inv.[NoteToInvitee],
+		inv.[WeddingParty],
+		inv.[DesignatedSeating],
 		rsvp.[Note],
 		guest.[Name],
 		guest.[Age],
@@ -13,7 +23,10 @@ as
 		guest.[AttendingReception],
 		rsvp.[DateTime] as RSVPDateTime
 	from [dbo].[Invitation] inv
-	inner join [dbo].[RSVP] rsvp
+	left join [dbo].[RSVP] rsvp
 		on inv.WorkItemId = rsvp.InviteeId
-	inner join [dbo].[Guest] guest
+	left join [dbo].[Guest] guest
 		on guest.[RsvpId] = rsvp.WorkItemId
+GO
+
+
